9c71cc55af1210fe8a751cf3a6ce06423616019e
BadreddineZatout <hb_zatout@esi.dz>
2024-03-17 17:45:13 +0100

    feat: user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'current_role',
     ];
 
     /**
@@ -33,6 +34,13 @@ class User extends Authenticatable
     ];
 
     /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * Get the attributes that should be cast.
      *
      * @return array<string, string>
@@ -40,7 +48,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
