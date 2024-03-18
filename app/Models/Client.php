c62aa56f432959ebac1d5b43407ca574dc2f9e8e
BadreddineZatout <hb_zatout@esi.dz>
2024-03-18 17:00:42 +0100

    feat: client resource

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     protected $fillable = ['first_name', 'last_name', 'phone'];
+
+    public function profile()
+    {
+        return $this->hasOne(ProfileUser::class);
+    }
 }
